46d450764cbf691dad3b7fe4b90c1ef869cc99cb
Tobias Schonrock <tobias@schonrocks.com>
2026-01-03 14:37:24 +0100

    working through database without pandas

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from dash import Dash, html, dcc, callback, Output, Input
+from pymongo import MongoClient
 import plotly.express as px
-import pandas as pd
 
-df = pd.read_csv('data.csv')
 app = Dash()
+
+uri = "mongodb://localhost:27017/?directConnection=true"
+client = MongoClient(uri)
+db = client.get_database("data")
+countries = db.get_collection("countries")
+
+countryNames = [c["name"] for c in countries.find({}, {"name": 1})]
 
 # Requires Dash 2.17.0 or later
 app.layout = [
     html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Dropdown(countryNames, 'Canada', id='dropdown-selection'),
     dcc.Graph(id='graph-content')
 ]
 
@@ -17,8 +23,11 @@ app.layout = [
     Input('dropdown-selection', 'value')
 )
 def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
+    res = countries.find_one({"name": {"$eq": value}}, {"data": 1})
+    xs = [d["pop"] for d in res["data"]]
+    ys = [d["year"] for d in res["data"]]
+    # print(xs)
+    return px.line({"pop": xs, "year": ys}, x='year', y='pop')
 
 if __name__ == '__main__':
     app.run(debug=False)
--- mongoTest.py
+++ mongoTest.py
@@ -5,8 +5,11 @@ client = MongoClient(uri)
 try:
     db = client.get_database("data")
     countries = db.get_collection("countries")
-    germ = countries.find_one({"name": {"$eq": "Germany"}})
-    print(germ)
+    res = countries.find({"name": {"$eq": "Germany"}}, {"name": 1})
+    res = countries.find({}, {"name": 1})
+    names = [c["name"] for c in res]
+    print(res)
+    print(names)
     client.close()
 except Exception as e:
     raise Exception("Unable to find the document due to the following error: ", e)
