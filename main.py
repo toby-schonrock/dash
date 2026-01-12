1195bed8d6ae6239676efcb8ae0e8a13acbc8e15
Tobias Schonrock <tobias@schonrocks.com>
2026-01-12 21:27:24 +0100

    dmc provider working

--- main.py
+++ main.py
@@ -10,23 +10,20 @@ db = client.get_database("data")
 countries = db.get_collection("countries")
 
 countryNames = [c["name"] for c in countries.find({}, {"name": 1})]
-
-# Requires Dash 2.17.0 or later
-app.layout = [
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(countryNames, 'Canada', id='dropdown-selection'),
+app.layout = dmc.MantineProvider([
+    html.H1(children='Country stats', style={'textAlign':'center'}),
+    dcc.Dropdown(countryNames, 'Canada', id='select-country'),
     dcc.Graph(id='graph-content')
-]
+])
 
 @callback(
     Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
+    Input('select-country', 'value')
 )
 def update_graph(value):
     res = countries.find_one({"name": {"$eq": value}}, {"data": 1})
     xs = [d["pop"] for d in res["data"]]
     ys = [d["year"] for d in res["data"]]
-    # print(xs)
     return px.line({"pop": xs, "year": ys}, x='year', y='pop')
 
 if __name__ == '__main__':
